e08b26d04232336470b094c60dfcbce1968b7696
Bruno <brunoteixeiralc@gmail.com>
2021-07-01 18:42:01 -0300

    UITest

--- InTargetTests/InTargetTests.swift
+++ InTargetTests/InTargetTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import NoAlvo
+@testable import InTarget
 
 class InTargetTests: XCTestCase {
 
--- InTargetUITests/InTargetUITests.swift
+++ InTargetUITests/InTargetUITests.swift
@@ -8,9 +8,34 @@
 import XCTest
 
 class InTargetUITests: XCTestCase {
-
-    func testExample() throws {
+    
+    func testMain() throws {
         let app = XCUIApplication()
         app.launch()
+        
+        let closeButton = app.buttons["close_btn"]
+        let accountButton = app.buttons["account_btn"]
+        let aboutButton = app.buttons["about_btn"]
+        let rankingButton = app.buttons["ranking_btn"]
+        let hitMeButton = app.buttons["hitMe_btn"]
+        let startOverButton = app.buttons["startOver_btn"]
+        
+        accountButton.tap()
+        
+        aboutButton.tap()
+        aboutButton.tap()
+        
+        closeButton.tap()
+        
+        rankingButton.tap()
+        sleep(2)
+        closeButton.tap()
+        
+        hitMeButton.tap()
+        app.buttons["OK"].tap()
+        
+        startOverButton.tap()
+        sleep(3)
+                                                
     }
 }
